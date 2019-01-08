400075cfdf9f2a4109e6c146300fdcf20bf54ee4
iftekhar209 <46313016+iftekhar209@users.noreply.github.com>
2019-01-08 23:40:21 +1000

    Add files via upload

---
+++ template-demo.php
@@ -0,0 +1,109 @@
+<?php /* Template Name: Demo Page Template */ get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+			
+			
+					
+			<div class="all-portfolio-content">
+			<div class="row work-content">
+				
+				<div class="five portfolio-description">
+					<div class="portfolio-heading-left">				
+					
+						<h1>heading1</h1>
+
+					</div>							
+							<h2>Client Feedback:</h2>
+							<p class="quote">"Chris was a wonderful person to work with. He is rare in that he has both the ability to create great design and genuine technical writing talents. Many times it can be difficult communicating the intricacies of your app with someone that only understands design, because you aren't selling designs, you need to speak to the needs of your customer, which means translating the value of your software with persuasive content and technical writing. That is exactly what Chris was able to deliver. I will continue to hire Chris for future projects because he made to easy to work with him.</p>
+ 
+ 							
+							<h2>Project Skills:</h2>
+							<ul>
+								<li>Web design and layout</li>
+								<li>HTML/CSS</li>
+								<li>Wordpress Theme Customization</li>
+								<li>Responsive design</li>
+								<li>Graphic design</li>
+								<li>Content development (technical and marketing)</li>
+							</ul>
+				</div>
+				
+				<div class="seven portfolio-image">
+				<div class="slider">
+				
+				<figure>
+					<img class="landscape" src="https://theme.valancers.com/wp-content/uploads/2019/01/scope4.png" alt="scope4 interface" />
+					<figcaption>Screen showcasing technical writing and graphic design</figcaption>
+				</figure>
+
+				
+				
+				</div>
+			</div>
+	
+
+		</div>
+				<div class="row work-content-top">
+				
+	
+						<div class="five image-description">
+						<div class="portfolio-heading-right">
+
+							<h1>heading2</h1>
+							
+						</div>
+						
+							<h2>Overview:</h2>
+							<p>The Department of Technical Communication runs semester-long client projects in which upper-level undergraduate courses partner with businesses on a client project. To manage these projects, the department needed a centralized repository where clients, instructors, and administrators could propose potential projects and the projects could be matched to potential undergraduate courses.</p>
+							<p>In response to this need, I built a custom system using HTML/CSS and Ruby on Rails to meet the department's needs. The system has full user administration capabilities as well as a variety of user access levels. This was by far my most challenging project, but also very rewarding. Sign up for an account to see my work!</p>
+							<h2>Project Skills:</h2>
+							<ul>
+								<li>HTML/CSS</li>
+								<li>Backend development - Ruby on Rails</li>
+								<li>User Interface Design</li>
+								<li>Responsive design using Twitter Bootstrap</li>
+								<li>Content strategy and writing</li>
+							</ul>
+						</div>
+						<div class="seven image">
+							<img src="https://theme.valancers.com/wp-content/uploads/2019/01/bold-four.png" alt="bold interface" />
+						</div>
+				</div>
+		
+			<div class="row work-content">
+				
+				<div class="five portfolio-description">
+					<div class="portfolio-heading-left">				
+					
+						<h1>heading3</h1>
+
+					</div>
+					
+							<h2>Overview:</h2>
+							<p>I created this website as a home base for my academic work as a professor. The site features a three color theme that is repeated throughout the pages of the site. It's meant to stand out and be unique in a world of boring, academic profile websites.</p>
+							<p>The most vital page to the site is the CV. Therefore, I created the CV in a readable web format so users can access the content across a variety of devices.</p>
+							<h2>Project Skills:</h2>
+							<ul>
+								<li>HTML/CSS</li>
+								<li>User Interface Design</li>
+								<li>Responsive design</li>
+								<li>Content strategy and writing</li>
+							</ul>
+				</div>
+				<div class="seven portfolio-image">
+						<img class="landscape" src="https://theme.valancers.com/wp-content/uploads/2019/01/portfolio-four.png" alt="portfolio interface" />
+				</div>
+			</div>
+		</div><!--all portfolio content-->
+			
+			
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
